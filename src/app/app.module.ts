61357c468ce80c006b1632c5ced4694d077b3c54
Girish D. Fuluskar <girish.fuluskar@gmail.com>
2020-04-19 15:18:27 +0530

    Code Submit
    Latest Code update on this project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
+import { IonicApp, IonicErrorHandler } from 'ionic-angular';
+
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { BluetoothSerial } from '@ionic-native/bluetooth-serial/ngx';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,6 +20,7 @@ import { AppComponent } from './app.component';
   providers: [
     StatusBar,
     SplashScreen,
+    BluetoothSerial,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
 
+import { BluetoothSerial } from '@ionic-native/bluetooth-serial/ngx';
+
+import { NavController } from 'ionic-angular';
+
+import { AlertController, ToastController } from '@ionic/angular';
+import { error } from 'util';
+
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -7,6 +15,128 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  pariredList: pairedList;
+  listToggle: boolean = false;
+  paireDeviceID: number = 0;
+  dataSend: string = "";
 
+  public navCtrl: NavController;
+  
+  
+
+  constructor(private bluetoothSerial: BluetoothSerial, public alertCtrl: AlertController,public toastCtrl: ToastController) {
+      this.checkBluetoothEnable();
+
+      
+  }
+
+    
+
+    checkBluetoothEnable(){
+      this.bluetoothSerial.isEnabled().then(
+        success => {
+          this.listPairedDevices();
+          
+        }, error => {
+          this.showError("Please Enable Bluetooth");
+        }
+      );
+    }
+
+    listPairedDevices(){
+      this.bluetoothSerial.list().then(success => {
+        this.pariredList = success;
+        console.log("List", this.pariredList);
+        this.listToggle = true;
+      }, error => {
+        this.showError("Please Enable Bluetooth");
+        this.listToggle = false;
+      })
+    }
+
+    selectDevice(){
+      let connectedDevice = this.pariredList[this.paireDeviceID];
+      if(!connectedDevice.address) {
+        this.showError("Select Paired Device to connect");
+        return;
+      }
+      let address = connectedDevice.address;
+      console.log("Connected Device: ", address);
+      let name = connectedDevice.name;
+      console.log("Connected device name: ", name);
+
+      this.connect(address);
+    }
+
+    connect(address: string){
+      this.bluetoothSerial.connect(address).subscribe(success => {
+        this.deviceConnected();
+        this.showToast("Successfully Connected");
+        console.log("Device connected: ", address);
+      }, error => {
+        this.showError("Error: Connecting to device");
+        console.log("Error connecting device: ", error);
+      });
+    }
+
+    deviceConnected(){
+      this.bluetoothSerial.subscribe('\n').subscribe(success => {
+        this.handleData(success);
+        this.showToast("Connected Successfully");
+        console.log("Data Showing: ", this.dataSend);
+      }, error => {
+        this.showError(error);
+        console.log("Error showing data: ", error);
+      });
+    }
+
+    
+
+    deviceDisconnected(){
+      this.bluetoothSerial.disconnect();
+      this.showToast("Device Disconnected");
+    }
+
+    handleData(data: any){
+      this.showToast(data);
+      console.log("Getting data: ", data);
+    }
+
+    sendData(){
+      this.dataSend+='\n';
+      this.showToast(this.dataSend);
+
+      this.bluetoothSerial.write(this.dataSend).then(success => {
+        this.showToast(success);
+        console.log("Writing data: ", this.dataSend);
+      }, error => {
+        this.showError(error);
+        console.log("Error writing data: ", error);
+      });
+    }
+
+    async showError(error: string){
+      const alert = await this.alertCtrl.create({
+        header: "Error",
+        message: error,
+        buttons: ['Dismiss']
+      });
+      alert.present();
+    }
+
+    async showToast(msj: string){
+      const toast = await this.toastCtrl.create({
+        message: msj,
+        duration: 2000
+      });
+
+      toast.present();
+    }
+}
+
+interface pairedList {
+  "class": number,
+  "id": string,
+  "address": string,
+  "name": string
 }
